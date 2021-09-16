d3820261d6a2a5ee6919aa81f48ebbe651348745
fajjarnr <zfajart@gmail.com>
2021-09-16 10:10:27 +0700

    admin check

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { authCheck } = require("../middleware/auth");
+const { authCheck, adminCheck } = require("../middleware/auth");
 
 const { createOrUpdateUser, currentUser } = require("../controller/auth");
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
